234d89239f0071529c46e8d53815e74ee63ccaef
shimh-develop <91943154@qq.com>
2017-12-29 09:22:14 +0800

    Webpack babel

---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+     ]
+   },
+   plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究')
+    ]
+};
